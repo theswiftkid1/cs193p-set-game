6a89f2db748404ea29c7def738d71bca7f114070
theswiftkid <59027313+theswiftkid1@users.noreply.github.com>
2020-08-10 21:08:39 +0200

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by theswiftkid_ on 8/10/20.
+//  Copyright © 2020 theswiftkid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
