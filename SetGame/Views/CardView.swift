263e4c7938569154bc7338d38cd90c2a4cf77dc4
theswiftkid <59027313+theswiftkid1@users.noreply.github.com>
2020-08-17 20:57:55 +0200

    Improve Card View
    Display shapes as plain, open or half opacity for
    the stripped option temporarily
    Improve sizing, padding
    Add shadow on selection

--- SetGame/Views/CardView.swift
+++ SetGame/Views/CardView.swift
@@ -11,11 +11,25 @@ import SwiftUI
 struct CardView: View {
     var card: GameModel.Card
 
-    func cardShape() -> AnyView {
-        switch card.shape {
-        case .Circle: return AnyView(Circle())
-        case .Ellipse: return AnyView(Ellipse())
-        case .Rectangle: return AnyView(Rectangle())
+    let strokeWidth: CGFloat = 4
+    var opacity: Double {
+        card.shading == .Striped ? 0.5 : 1
+    }
+
+    var cardShape: some View {
+        switch (card.shape, card.shading) {
+        case (.Circle, .Open):
+            return AnyView(Circle().stroke(lineWidth: strokeWidth))
+        case (.Circle, _):
+            return AnyView(Circle())
+        case (.Ellipse, .Open):
+            return AnyView(Ellipse().stroke(lineWidth: strokeWidth))
+        case (.Ellipse, _):
+            return AnyView(Ellipse())
+        case (.Rectangle, .Open):
+            return AnyView(Rectangle().stroke(lineWidth: strokeWidth))
+        case (.Rectangle, _):
+            return AnyView(Rectangle())
         }
     }
 
@@ -23,19 +37,34 @@ struct CardView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Color(card.color), lineWidth: 4)
-            )
-                .overlay(
-                    ForEach(0..<card.number) { _ in
-                        self.cardShape()
-                            .foregroundColor(Color(self.card.color))
-                            .padding()
+                .shadow(color: .black, radius: card.isSelected ? 10 : 0, x: 0, y: 0)
+
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color(card.color), lineWidth: 4)
+                .overlay (
+                    VStack {
+                        ForEach(0..<card.number) { _ in
+                            self.cardShape
+                                .foregroundColor(Color(self.card.color))
+                                .opacity(self.opacity)
+                                .padding(.horizontal)
+                        }
                     }
+                    .padding(.vertical)
             )
-//                .shadow(color: Color.gray, radius: 10, x: 5, y: 5)
-                .padding()
         }
+        .padding()
+        
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static let game = GameViewModel()
+
+    static var previews: some View {
+        game.dealCards(1)
+        let card = game.dealtCards.first!
+        return CardView(card: card)
+            .previewLayout(.fixed(width: 200, height: 300))
     }
 }
